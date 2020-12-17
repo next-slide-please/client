4e7b427cd4cfdac1af7f5edc8b985693ff6e15da
Lucas Jenss <public@x3ro.de>
2020-12-17 01:02:11 +0100

    Basic client functionality now working (except sending keyboard events)

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,30 @@
-use std::time::Instant;
-use std::thread;
-use std::time::Duration;
-
-extern crate reqwest;
-
-use druid::widget::prelude::*;
-use druid::{AppLauncher, Color, Selector, Target, Data, WidgetExt, Lens, WindowDesc, FontDescriptor, UnitPoint, LocalizedString, FontFamily};
-use druid::widget::{Label, TextBox, Flex, Controller, Align};
-use tungstenite::{connect, Message};
-use url::Url;
-use std::collections::HashMap;
-
 #[macro_use]
 extern crate log;
+extern crate reqwest;
 
-#[derive(Clone, Data, Lens)]
+
+use druid::{AppLauncher, Data, FontDescriptor, FontFamily, Lens, LocalizedString, UnitPoint, WidgetExt, WindowDesc};
+use druid::widget::{Align, Controller, Flex, Label, Button};
+use druid::widget::prelude::*;
+use std::thread;
+
+mod websocket;
+
+
+
+#[derive(Clone, Data, Lens, Debug)]
 struct AppState {
-    name: String,
-    backend_url: String,
+    websocket_url: Option<String>,
+    publish_url: Option<String>,
+    status: String,
 }
 
-const VERTICAL_WIDGET_SPACING: f64 = 20.0;
-const TEXT_BOX_WIDTH: f64 = 200.0;
-const WINDOW_TITLE: LocalizedString<AppState> = LocalizedString::new("Next slide please!");
 
-// If you want to submit commands to an event sink you have to give it some kind
-// of ID. The selector is that, it also assures the accompanying data-type is correct.
-// look at the docs for `Selector` for more detail.
-const SET_FOO: Selector<u32> = Selector::new("event-example.set-color");
+
+const VERTICAL_WIDGET_SPACING: f64 = 20.0;
+const WINDOW_TITLE: LocalizedString<AppState> = LocalizedString::new("Next slide please?");
+
+
 
 pub fn main() {
     env_logger::init();
@@ -39,17 +36,18 @@ pub fn main() {
 
     // create the initial app state
     let initial_state = AppState {
-        name: "World".into(),
-        backend_url: "http://127.0.0.1:8000".into(),
+        websocket_url: None,
+        publish_url: None,
+        status: "Initializing".into(),
     };
 
-    // start the application
+    // Spawn the thread that manages the websocket connection to the backend
     let launcher = AppLauncher::with_window(main_window);
     let event_sink = launcher.get_external_handle();
-    thread::spawn(move || websocket_thread(event_sink));
+    thread::spawn(move || websocket::WebSocketConnection::new(event_sink).connect_loop());
 
+    // start the application
     launcher
-       // .use_simple_logger()
         .launch(initial_state)
         .expect("Failed to launch application");
 }
@@ -58,20 +56,26 @@ pub fn main() {
 struct AppController;
 
 impl Controller<AppState, Align<AppState>> for AppController {
-    fn event(
-        &mut self,
-        child: &mut Align<AppState>,
-        ctx: &mut EventCtx,
-        event: &Event,
-        data: &mut AppState,
-        env: &Env,
-    ) {
+    fn event(&mut self, child: &mut Align<AppState>,  ctx: &mut EventCtx,  event: &Event,  data: &mut AppState, env: &Env) {
         match event {
-            Event::Command(cmd) if cmd.is(SET_FOO)=> {
-                let foo = cmd.get_unchecked(SET_FOO);
-                data.name = format!("foo {}", foo);
+            Event::Command(cmd) if cmd.is(websocket::STATE_CHANGED)=> {
+                match cmd.get_unchecked(websocket::STATE_CHANGED) {
+                    websocket::StateChange::Connecting => {
+                        data.status = "Connecting...".into();
+                        data.websocket_url = None;
+                        data.publish_url = None;
+                    }
 
-                //child.event(ctx, event, data, env)
+                    websocket::StateChange::Connected { websocket_url, publish_url } => {
+                        data.status = "Connected!".into();
+                        data.websocket_url = Some(websocket_url.to_owned());
+                        data.publish_url = Some(publish_url.to_owned());
+                    }
+
+                    websocket::StateChange::EventReceived(event) => {
+                        debug!("event received {:?}", event);
+                    }
+                }
             }
 
             _ => child.event(ctx, event, data, env),
@@ -81,67 +85,38 @@ impl Controller<AppState, Align<AppState>> for AppController {
 
 fn build_root_widget() -> impl Widget<AppState> {
     // a label that will determine its text based on the current app data.
-    let label = Label::new(|data: &AppState, _env: &Env| {
-        if data.name.is_empty() {
-            "Hello anybody!?".to_string()
-        } else {
-            format!("Hello {}!", data.name)
-        }
+    let label = Label::new(|_data: &AppState, _env: &Env| {
+        "Next slide please?".to_string()
     })
         .with_font(FontDescriptor::new(FontFamily::SERIF).with_size(32.0))
         .align_horizontal(UnitPoint::CENTER);
 
-    // a textbox that modifies `name`.
-    let textbox = TextBox::new()
-        .with_placeholder("Who are we greeting?")
-        .with_text_size(18.0)
-        .fix_width(TEXT_BOX_WIDTH)
-        .align_horizontal(UnitPoint::CENTER)
-        .lens(AppState::name);
+    let status_label = Label::<AppState>::new(|data: &AppState, _env: &Env| {
+        format!("Status: {}", data.status)
+    });
 
-    // arrange the two widgets vertically, with some padding
+    let open_publish_button = Button::<AppState>::new("Open control website")
+        .on_click(|_ctx, data, _env| {
+            if let Some(url) = &data.publish_url {
+                webbrowser::open(&url).expect("Failed to open browser");
+            }
+        });
+
+    // let last_action_label = Label::<AppState>::new(|_data: &AppState, _env: &Env| {
+    //    format!("Last action:")
+    // })
+    //     .with_font(FontDescriptor::new(FontFamily::SERIF).with_size(16.0))
+    //     .align_horizontal(UnitPoint::CENTER)
+    //     .on_click(|_ctx, _data, _env| {
+    //
+    //     });
+
     Flex::column()
         .with_child(label)
         .with_spacer(VERTICAL_WIDGET_SPACING)
-        .with_child(textbox)
+        .with_child(status_label)
+        .with_spacer(VERTICAL_WIDGET_SPACING)
+        .with_child(open_publish_button)
         .align_vertical(UnitPoint::CENTER)
         .controller(AppController)
-}
-
-fn websocket_thread(event_sink: druid::ExtEventSink) {
-    let client = reqwest::blocking::Client::new();
-    let mut res = client.post("http://127.0.0.1:8000/register")
-        .header("Content-Type", "application/json")
-        .body("{}")
-        .send()
-        .unwrap();
-    let content = res.json::<HashMap<String, String>>().unwrap();
-    let url = content.get("url").unwrap();
-    println!("{:?}", url);
-
-    let (mut socket, response) =
-        connect(Url::parse(url).unwrap()).expect("Can't connect");
-
-    println!("Connected to the server");
-    println!("Response HTTP code: {}", response.status());
-    println!("Response contains the following headers:");
-    for (ref header, _value) in response.headers() {
-        println!("* {}", header);
-    }
-
-    //socket.write_message(Message::Text("Hello WebSocket".into())).unwrap();
-    loop {
-        let msg = socket.read_message().expect("Error reading message");
-        println!("Received: {}", msg);
-    }
-
-    loop {
-        // if event_sink
-        //         .submit_command(SET_FOO, 5, Target::Auto)
-        //         .is_err()
-        //     {
-        //         break;
-        //     }
-        thread::sleep(Duration::from_millis(1000));
-    }
 }
---
+++ src/websocket.rs
@@ -0,0 +1,145 @@
+use druid::{Selector, Target};
+use std::collections::HashMap;
+use tungstenite::{connect, WebSocket, Message};
+use reqwest::Url;
+use std::thread;
+use std::time::Duration;
+use std::error::Error;
+use tungstenite::client::AutoStream;
+
+#[derive(Clone, Debug)]
+pub enum Event {
+    Previous,
+    Next,
+}
+
+#[derive(Clone, Debug)]
+pub enum StateChange {
+    Connecting,
+    Connected { websocket_url: String, publish_url: String },
+    EventReceived(Event),
+}
+
+const BACKEND_REGISTER_ENDPOINT: &str = "http://next-slide-please.com:8000/register";
+pub(crate) const STATE_CHANGED: Selector<StateChange> = Selector::new("set-websocket-state");
+
+
+pub struct WebSocketConnection {
+    event_sink: druid::ExtEventSink,
+    client: reqwest::blocking::Client,
+}
+
+impl WebSocketConnection {
+    pub fn new(event_sink: druid::ExtEventSink) -> Self {
+        WebSocketConnection {
+            event_sink,
+            client: reqwest::blocking::Client::new()
+        }
+    }
+
+    pub fn submit_command(&self, payload: StateChange) {
+        self.event_sink.submit_command(
+            STATE_CHANGED,
+            payload,
+            Target::Auto
+        ).expect("Failed to send message to main thread");
+    }
+
+    fn register(&self) -> Result<(String, String), Box<dyn Error>> {
+        let res = self.client.post(BACKEND_REGISTER_ENDPOINT)
+            .header("Content-Type", "application/json")
+            .body("{}")
+            .send()?;
+
+        let content = res.json::<HashMap<String, String>>()?;
+        let publish_url = content.get("publish_url")
+            .ok_or("Response did not contain 'publish_url'")?
+            .to_owned();
+        let websocket_url = content.get("websocket_url")
+            .ok_or("Response did not contain 'websocket_url'")?
+            .to_owned();
+
+        debug!("Websocket URL: {:?}", websocket_url);
+        debug!("Publish URL: {:?}", publish_url);
+
+        Ok((publish_url, websocket_url))
+    }
+
+    fn websocket_connect(&self, websocket_url: &String) -> Result<WebSocket<AutoStream>, Box<dyn Error>> {
+        let url = Url::parse(websocket_url)?;
+        let (socket, response) = connect(url)?;
+
+        debug!("Connected to the server");
+        debug!("Response HTTP code: {}", response.status());
+        debug!("Response contains the following headers:");
+        for (ref header, _value) in response.headers() {
+            debug!("* {}", header);
+        }
+
+        Ok(socket)
+    }
+
+    fn connect(&mut self) {
+        self.submit_command(StateChange::Connecting);
+
+        let (publish_url, websocket_url) = match self.register() {
+            Err(e) => {
+                error!("Failed to register new connection: {}", e);
+                return;
+            }
+            Ok((publish_url, websocket_url)) => (publish_url, websocket_url)
+        };
+
+        let mut socket = match self.websocket_connect(&websocket_url) {
+            Err(e) => {
+                error!("Failed to establish websocket connection: {}", e);
+                return;
+            }
+            Ok(socket) => socket
+        };
+
+        self.submit_command(StateChange::Connected {
+            publish_url: publish_url.to_owned(),
+            websocket_url: websocket_url.to_owned()
+        });
+
+
+
+        //socket.write_message(Message::Text("Hello WebSocket".into())).unwrap();
+        loop {
+            match socket.read_message() {
+                Err(e) => {
+                    debug!("Error reading from websocket {:?}", e);
+                    break;
+                }
+                Ok(Message::Text(msg)) if msg == "next" => {
+                    debug!("'Next' received!");
+                    self.submit_command(StateChange::EventReceived(Event::Next));
+                }
+
+                Ok(Message::Text(msg)) if msg == "prev" => {
+                    debug!("'Previous' received!");
+                    self.submit_command(StateChange::EventReceived(Event::Previous));
+
+                }
+
+                Ok(msg) => {
+                    debug!("Received unexpected message: {}", msg)
+                }
+            }
+        }
+    }
+
+
+    pub fn connect_loop(&mut self) {
+        loop {
+            info!("Trying to establish websocket connection");
+            self.connect();
+            thread::sleep(Duration::from_secs(1));
+        }
+    }
+
+    // fn websocket_register(event_sink: druid::ExtEventSink) -> Result<(), Box<dyn std::error::Error>> {
+    //     Ok(())
+    // }
+}
